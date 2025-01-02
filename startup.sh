0bc655be6a95115246678beb24d33a3beb464588
Anatoli Eckert <anatolieckert@gmail.com>
2025-01-02 14:35:50 +0100

    up

--- startup.sh
+++ startup.sh
@@ -2,16 +2,15 @@
 
 set -e
 
-## 1) Start Cloudflare WARP daemon in the background
-#echo "Starting warp-svc in the background..."
-#nohup warp-svc > /dev/null 2>&1 &
-## 2) Wait a moment to ensure warp-svc is running
-#echo "Waiting 5s to ensure warp-svc is running..."
-#sleep 5
-## 3) Automatically accept TOS, register, and connect
-#yes y | warp-cli registration new --accept-tos
-#warp-cli connect
-
+# 1) Start Cloudflare WARP daemon in the background
+echo "Starting warp-svc in the background..."
+nohup warp-svc > /dev/null 2>&1 &
+# 2) Wait a moment to ensure warp-svc is running
+echo "Waiting 2s to ensure warp-svc is running..."
+sleep 2
+# 3) Automatically accept TOS, register, and connect
+warp-cli --accept-tos registration new
+warp-cli --accept-tos connect
 
 # 4) Start your primary app (Flask, etc.)
 echo "Starting Flask app..."
