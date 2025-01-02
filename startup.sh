7e70e43f7db237ad12c5d964c4de688f8d64f7c8
Anatoli Eckert <anatolieckert@gmail.com>
2025-01-02 13:14:31 +0100

    up

--- startup.sh
+++ startup.sh
@@ -9,9 +9,9 @@ sleep 2
 
 # 3) Automatically accept TOS, register, and connect
 #    (If "warp-cli register --accept-tos" is sufficient for you, use that)
-yes y | warp-cli registration new --accept-tos
-warp-cli mode proxy
-warp-cli connect
+#yes y | warp-cli registration new --accept-tos
+#warp-cli mode proxy
+#warp-cli connect
 
 # 4) Start your primary app (Flask, etc.)
 #    Use 'exec' so your process gets signals directly
